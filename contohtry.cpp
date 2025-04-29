1e1f3a9eb7fff581f78787b702f57fc1acc07d9a
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-04-29 18:24:10 +0700

    Membuat Akses Modifier Public

---
+++ contohtry.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+    int nim;
+    string nama;
+    float nilai;
+
+void printData(){
+    cout << "Nim :"<< nim <<endl;
+    cout << "Nama :"<< nama <<endl;
+    cout << "Nim :"<< nilai <<endl;
+}
+
+int main(){
+
+    nim = 20240140111;
+    nama = "Riza";
+    nilai = 100;
+
+}
